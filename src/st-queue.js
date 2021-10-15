c85107a744f86de7bc2266af127e8b0e141672b0
Nikita1814 <kravchenko1814@gmail.com>
2021-10-15 20:50:42 +0300

    feat : add queue function

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
+    return this.list
+    }
+  
+    enqueue(value) {
+    if (this.list === undefined){
+      this.list =  new ListNode(value)
+    } else {
+      let current = this.list
+      while(current.next){
+        current = current.next 
+      }
+      current.next = new ListNode(value)
+    }
+    }
+  
+    dequeue() {
+    let val = this.list.value
+    this.list = this.list.next
+    return val
+    }
 }
